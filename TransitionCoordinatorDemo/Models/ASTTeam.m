761eeaf870b3f76ac834bf024e7ee39004e6bb1e
Bill Williams <bill@flpatriot.com>
2014-04-18 23:22:37 -0700

    Fix hex color parser.

--- TransitionCoordinatorDemo/Models/ASTTeam.m
+++ TransitionCoordinatorDemo/Models/ASTTeam.m
@@ -12,7 +12,6 @@
 UIColor *UIColorFromHexString(NSString *hexString) {
     unsigned rgbValue = 0;
     NSScanner *scanner = [NSScanner scannerWithString:hexString];
-    [scanner setScanLocation:1]; // bypass '#' character
     [scanner scanHexInt:&rgbValue];
     return [UIColor colorWithRed:((rgbValue & 0xFF0000) >> 16)/255.0 green:((rgbValue & 0xFF00) >> 8)/255.0 blue:(rgbValue & 0xFF)/255.0 alpha:1.0];
 }
